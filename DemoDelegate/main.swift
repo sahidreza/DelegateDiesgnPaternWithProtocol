dabe71a3729b861b1f5cfe8de2316e99bc4507e4
sahidcse15@gmail.com <sahidcse15@gmail.com>
2022-12-22 17:30:21 +0530

    Initial Commit

--- DemoDelegate/main.swift
+++ DemoDelegate/main.swift
@@ -7,5 +7,50 @@
 
 import Foundation
 
-print("Hello, World!")
+protocol ResultDelegate{
+    
+    func resltOfSumProcessing(result:Int)
+}
+
+class Manager: ResultDelegate {
+
+    var developer:Developer?
+
+    init(developer: Developer) {
+        self.developer = developer
+        developer.delegate = self
+    }
+
+    func twoNumberSum(a:Int,b:Int){
+
+       developer?.doingSum(x: a, y: b)
+
+    }
+
+    func resltOfSumProcessing(result:Int){
+        print("HEllo This the reslut",result)
+    }
+}
+
+class Developer{
+
+    var delegate:ResultDelegate?
+
+    func doingSum(x:Int,y:Int)  {
+
+        let sum = x + y
+        delegate?.resltOfSumProcessing(result: sum)
+
+    }
+    
+    
+
+
+}
+
+let ob1 = Manager(developer: Developer())
+ob1.twoNumberSum(a: 9, b: 9)
+
+
+
 
